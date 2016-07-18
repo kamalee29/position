212c1083822419795ad353848a2b34136bd5b3dd
kamalee29 <kamalee.selvii24@gmail.com>
2016-07-18 01:38:38 -0700

    Update reversestring.java

--- reversestring.java
+++ reversestring.java
@@ -4,6 +4,6 @@
         Scanner sc=new Scanner(System.in);
         System.out.println("enter thebString to be reversed");
         s1=sc.nextLine();
-     String str=new StringBuffer(s).reverse().toString();
+     String str=new StringBuffer(s1).reverse().toString();
         System.out.println("the reversable String is "+str);
     }
