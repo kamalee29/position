afd26c370737afc2fcbf435d625956a1c95007a5
kamalee29 <kamalee.selvii24@gmail.com>
2016-07-18 01:33:27 -0700

    Add files via upload

---
+++ reversestring.java
@@ -0,0 +1,9 @@
+
+  public static void main(String[] args) {
+        String s1;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter thebString to be reversed");
+        s1=sc.nextLine();
+     String str=new StringBuffer(s).reverse().toString();
+        System.out.println("the reversable String is "+str);
+    }
